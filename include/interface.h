875c9909aa69eff4c2169fdd428b8c321ba0efe4
MamadovZaki <mamadovzicx@gmail.com>
2021-01-01 20:08:08 +0200

    Setup headers | Added utility functions header file and program interface header

---
+++ include/interface.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define STATUS_SUCCESS 1
+#define STATUS_FAILED 0
+#define STATUS_UNDEFINED -1
---
+++ include/utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1000
+extern int errno;
