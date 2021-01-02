3103afa76ed59974da700a53893830e1edf712c5
MamadovZaki <mamadovzicx@gmail.com>
2021-01-02 03:55:16 +0200

    Uitlities | input_str(), generate_rand_int()
    Defined and implemented:
        input_str(char*) to accept string input from user
        generate_rand_int(int, int) generates a 6 digit pseudo-random integer

--- include/utils.h
+++ include/utils.h
@@ -11,4 +11,37 @@
 #define BUFFER_SIZE 1000
 extern int errno;
 
+/*******************************************************
+     * Inputs: 
+     **********
+     * Message prompts user to input an integer value.
+     
+     * Description:
+     *************** 
+     * prompts a message to user to enter an integer value.
+     
+     * Output: 
+     ***********
+     * Returns the integer input from user. 
+     * Returns 0 if user didn't input a valid value.
+     *******************************************************/
 int input_int(char *);
+
+/*******************************************************
+     * Inputs: 
+     **********
+     * Message prompts user to input a string value.
+     
+     * Description:
+     *************** 
+     * prompts a message to user to enter a string and returns a pointer to this string.
+     
+     * Output: 
+     ***********
+     * Returns the string inputted by user. 
+     * Returns NULL if memo allocation failed.
+     *******************************************************/
+char *input_str(char *);
+
+/* generates a rand int */
+int generate_rand_id(int, int);
--- src/utils.c
+++ src/utils.c
@@ -2,36 +2,13 @@
 
 int input_int(char *msg)
 {
-    /***********************************************************************************
-     * Inputs: 
-     **********
-     * Message prompts user to input an integer value.
-     
-     * Description:
-     *************** 
-     * prompts a message to user
-     * it takes string input from user and store it in a buffer 
-     * stripes '\n' from the string 
-     * dynamically allocate a new string and copies the value from buffer 
-     * Note: Why using dynamic allocation? 
-     *  We can't return buffer directly since it is a local variable 
-     * 
-     * When user inputs nothing but a newline (return key), the function returns 0
-     *  !IT IS IMPORTAT TO FREE MEMORY AFTER USING THIS FUNCTION TO AVOID MEM LEAKS  
-     * 
-     * Output: 
-     ***********
-     * Returns the integer input from user 
-     * Returns 0 if user didn't input any value 
-     ***********************************************************************************/
-
     printf("%s", msg);
 
-    char buf[10];
+    char buf[100];
     int id;
 
     fgets(buf, sizeof(buf), stdin);
-    buf[strcspn(buf, "\n")] = 0;
+    buf[strcspn(buf, "\n")] = 0; // remove newline character
 
     // validates user input
     if (strcmp(buf, "") == 0)
@@ -40,4 +17,37 @@ int input_int(char *msg)
     id = strtol(buf, NULL, 10);
 
     return id;
+}
+
+char *input_str(char *msg)
+{
+    char *str;
+    char buffer[100];
+    int buffer_size = sizeof(buffer);
+
+    printf("%s", msg);
+
+    /* accepts string input from user */
+    fgets(buffer, buffer_size, stdin);
+    /* remove "\n" from the string */
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    /* check if user has entered an empty string */
+    if ((strcmp(buffer, "")) == 0)
+        return NULL;
+
+    /* allocate memory space for the new string, sized to the content of the buffer*/
+    /* Extra byte added to allocate \0 */
+    str = malloc(strlen(buffer) + 1);
+    /* copy the trimmed content up to \0 to the new string */
+    strcpy(str, buffer);
+
+    return str;
+}
+
+int generate_rand_id(int MAX, int MIN)
+{
+    srand(time(NULL));
+    int randomID = rand() % MAX + MIN;
+    return randomID;
 }
