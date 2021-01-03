2844da3de22e20e0155f8c87a102b1bd3a0c1a94
MamadovZaki <mamadovzicx@gmail.com>
2021-01-03 04:15:22 +0200

    Test | Testing util function input_str(char*)

--- test/test.c
+++ test/test.c
@@ -3,8 +3,13 @@
 int main(int argc, char const *argv[])
 {
     UNITY_BEGIN();
-    printf("---------- Testing input_int() ---------------\n");
-    RUN_TEST(test_input_int);
+    //printf("---------- Testing input_int() ---------------\n");
+    //RUN_TEST(test_input_int);
+
+    printf("\n");
+
+    printf("---------- Testing input_str() ---------------\n");
+    RUN_TEST(test_input_str);
 
     return UNITY_END();
 }
--- test/test.h
+++ test/test.h
@@ -18,3 +18,12 @@ void test_input_int(void)
     //printf("\nPlease input a number greater than 10 digits that func produces 0\n");
     //TEST_ASSERT_EQUAL(0, input_int("Please add an integer\n"));
 }
+
+void test_input_str(void)
+{
+    printf("\ninput: \"Hello World!\" Expected output: \"Hello World!\" \n");
+    TEST_ASSERT_EQUAL_STRING("Hello World!", input_str("string: \n"));
+
+    printf("\ninput: \"\" Expected output: NULL \n");
+    TEST_ASSERT_EQUAL_STRING(NULL, input_str("string: \n"));
+}
